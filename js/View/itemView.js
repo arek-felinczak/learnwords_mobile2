65deca1c7371877fd2727f279ab638963be9b190
arek-felinczak <arek_felinczak@o2.pl>
2013-09-20 15:38:48 +0200

    broken commit

--- js/View/itemView.js
+++ js/View/itemView.js
@@ -3,8 +3,9 @@ ItemView = Backbone.View.extend({
 	 render:function (nr) {
         var word = nr === "1" ? this.model.get('Translation1') : this.model.get('Translation2');
         var url = this.buildLink(word, window.localStorage['dictionaryLink']);
-        $("iframe#DictionaryItemView").attr('src', url);
-        $('div#ItemViev').show();
+        var vm = {src: url};
+        var html = this.template(vm);
+        return html;
 	 },
      
      buildLink:function (word, engineUrl) {
--- js/View/itemsView.js
+++ js/View/itemsView.js
@@ -1,15 +1,8 @@
 ItemsView = Backbone.View.extend({
       
 	 render:function (eventName) {
-	    var source = $("#items-template").html();
-	    var template = Handlebars.compile(source);
-	    
-        if (this.model.models.length == 0){
-            return "No items found."
-        }
-        
-        var vm = {category: this.model.toJSON(), count: this.model.models.length};
-	    var html = template(vm);
+	    var vm = {category: this.model.toJSON(), count: this.model.models.length};
+	    var html = this.template(vm);
         return html;
 	 }
 	 
