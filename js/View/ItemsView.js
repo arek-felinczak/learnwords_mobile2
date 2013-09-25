d5e3449625eaf0c42dcb1aa316eec81a83c1aa5e
arek-felinczak <arek_felinczak@o2.pl>
2013-09-25 11:55:58 +0200

    rename

---
+++ js/View/ItemsView.js
@@ -0,0 +1,9 @@
+ItemsView = Backbone.View.extend({
+      
+	 render:function (eventName) {
+        this.template = window.templates['ItemsView'];
+	    var vm = {category: this.model.toJSON(), count: this.model.models.length};
+	    var html = this.template(vm);
+        return html;
+	 }
+});
